aeaa78df7581519a9245185c33a1bf7a22b65f29
DenseLance-alt <e1406571@u.nus.edu>
2026-01-19 17:10:29 +0800

    Level 4: Add new tasks
    Tasks are too generic.
    
    There are many other tasks, such as to-do, deadlines, and events.
    
    Let's,
    * add different types of tasks

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,16 @@
+public class Deadline extends Task {
+    private String by;
+
+    public Deadline(String name, String by) {
+        super(name);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "[D]%s (by: %s)",
+                super.toString(),
+                this.by);
+    }
+}
