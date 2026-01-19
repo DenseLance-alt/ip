30b34d6b1d438a4899da0c4ee8e97210a294f0e9
DenseLance-alt <e1406571@u.nus.edu>
2026-01-19 15:22:43 +0800

    Level 2: Add functionality for storing and listing user commands
    Chatbot currently lacks functionality.
    
    Store user commands so that user can display them back when requested.
    
    Let's,
    * include the list command to display all stored user commands
    * store other commands in the chatbot's memory

--- src/main/java/YoshikageKira.java
+++ src/main/java/YoshikageKira.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class YoshikageKira {
     public static void main(String[] args) {
@@ -10,16 +11,31 @@ public class YoshikageKira {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> textList = new ArrayList<>();
         while (true) {
             String command = scanner.nextLine();
+
+            // Exit Program
             if (command.equals("bye")) {
                 break;
             }
+
+            // Respond to Other Commands
             System.out.println(separator);
-            System.out.println("\t" + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < textList.size(); i++) {
+                    System.out.println(
+                            String.format("\t%d. %s", i + 1, textList.get(i)));
+                }
+            } else {
+                textList.add(command);
+                System.out.println("\tadded: " + command);
+            }
             System.out.println(separator);
         }
+
         scanner.close();
+        System.out.println(separator);
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(separator);
     }
