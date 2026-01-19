4ea7207c42d21400fad2c33dc0f265414f5f1d6c
DenseLance-alt <e1406571@u.nus.edu>
2026-01-19 15:00:46 +0800

    Level 1: Make chatbot echo command
    Chatbot exits immediately after greeting user.
    
    Chatbot should be able to listen and react to user command.
    
    Let's,
    * make chatbot repeat command provided by user
    * make chatbot exit when user types command "bye"

--- src/main/java/YoshikageKira.java
+++ src/main/java/YoshikageKira.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class YoshikageKira {
     public static void main(String[] args) {
-        String separator = "____________________________________________________________";
+        String separator = "\t____________________________________________________________";
+
         System.out.println(separator);
-        System.out.println("Hello! I'm Yoshikage Kira");
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm Yoshikage Kira");
+        System.out.println("\tWhat can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println(separator);
+            System.out.println("\t" + command);
+            System.out.println(separator);
+        }
+        scanner.close();
+        System.out.println("\tBye. Hope to see you again soon!");
         System.out.println(separator);
     }
 }
