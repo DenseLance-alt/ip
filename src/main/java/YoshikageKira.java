0369a7207915dc056d07675e412cd4ba99463d9c
DenseLance-alt <e1406571@u.nus.edu>
2026-01-19 20:21:02 +0800

    Optional: Refactor by renaming variable

--- src/main/java/YoshikageKira.java
+++ src/main/java/YoshikageKira.java
@@ -19,35 +19,35 @@ public class YoshikageKira {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
         while (!done) {
-            String command = scanner.nextLine();
+            String input = scanner.nextLine();
 
             // Respond to Commands
             System.out.println(separator);
-            if (command.equals("bye")) {
+            if (input.equals("bye")) {
                 done = true;
                 System.out.println("\tBye. Hope to see you again soon!");
-            } else if (command.equals("list")) {
+            } else if (input.equals("list")) {
                 System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0; i < taskList.size(); i++) {
                     System.out.println(
                             String.format("\t%d.%s", i + 1, taskList.get(i)));
                 }
-            } else if (command.startsWith("mark ")) {
-                String[] segments = command.split(" ", 2);
+            } else if (input.startsWith("mark ")) {
+                String[] segments = input.split(" ", 2);
                 int taskNumber = Integer.parseInt(segments[1]);
                 Task task = taskList.get(taskNumber - 1);
                 task.markTask();
                 System.out.println("\tNice! I've marked this task as done:");
                 System.out.println("\t  " + task);
-            } else if (command.startsWith("unmark ")) {
-                String[] segments = command.split(" ", 2);
+            } else if (input.startsWith("unmark ")) {
+                String[] segments = input.split(" ", 2);
                 int taskNumber = Integer.parseInt(segments[1]);
                 Task task = taskList.get(taskNumber - 1);
                 task.unmarkTask();
                 System.out.println("\tOK, I've marked this task as not done yet:");
                 System.out.println("\t  " + task);
-            } else if (command.startsWith("todo ")) {
-                String[] segments = command.split(" ", 2);
+            } else if (input.startsWith("todo ")) {
+                String[] segments = input.split(" ", 2);
                 ToDo task = new ToDo(segments[1]);
                 taskList.add(task);
                 System.out.println("\tGot it. I've added this task:");
@@ -55,8 +55,8 @@ public class YoshikageKira {
                 System.out.println(String.format(
                         "\tNow you have %d tasks in the list.",
                         taskList.size()));
-            } else if (command.startsWith("deadline ")) {
-                String[] segments = command.split(" ", 2);
+            } else if (input.startsWith("deadline ")) {
+                String[] segments = input.split(" ", 2);
                 segments = segments[1].split(" /by ", 2);
                 Deadline task = new Deadline(segments[0], segments[1]);
                 taskList.add(task);
@@ -65,8 +65,8 @@ public class YoshikageKira {
                 System.out.println(String.format(
                         "\tNow you have %d tasks in the list.",
                         taskList.size()));
-            } else if (command.startsWith("event ")) {
-                String[] segments = command.split(" ", 2);
+            } else if (input.startsWith("event ")) {
+                String[] segments = input.split(" ", 2);
                 segments = segments[1].split(" /from ", 2);
                 String event = segments[0];
                 segments = segments[1].split(" /to ", 2);
@@ -78,9 +78,9 @@ public class YoshikageKira {
                         "\tNow you have %d tasks in the list.",
                         taskList.size()));
             } else {
-                Task task = new Task(command);
+                Task task = new Task(input);
                 taskList.add(task);
-                System.out.println("\tadded: " + command);
+                System.out.println("\tadded: " + input);
             }
             System.out.println(separator);
         }
