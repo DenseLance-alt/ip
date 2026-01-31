25b66d507508f8ce46b40c8f6578d9b67d245447
DenseLance-alt <e1406571@u.nus.edu>
2026-01-31 10:29:48 +0800

    Level 10: Close app when sending bye command
    App does not close when bye command is sent.
    
    Let's,
    * make app close using an alternative method

--- src/main/java/chatbot/ui/Ui.java
+++ src/main/java/chatbot/ui/Ui.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
  */
 public class Ui {
     // Adapted from the copypasta https://www.reddit.com/r/copypasta/comments/mmaq30/my_name_is_yoshikage_kira/
+    public static final String FAREWELL = "\tNo... No, No, No! Where are they taking me!? "
+            + System.lineSeparator()
+            + "\tThey're dragging me away! Nooo!";
     private static final String INTRODUCTION = "\tMy name is Yoshikage Kira. I'm 33 years old. My house is in "
             + System.lineSeparator()
             + "\tthe northeast section of Morioh, where all the villas are, "
@@ -42,9 +45,6 @@ public class Ui {
             + "\thappiness. Although, if I were to fight I wouldn't lose to "
             + System.lineSeparator()
             + "\tanyone. ";
-    private static final String FAREWELL = "\tNo... No, No, No! Where are they taking me!? "
-            + System.lineSeparator()
-            + "\tThey're dragging me away! Nooo!";
     private static final String SEPARATOR = "\t____________________________________________________________";
 
     private Scanner scanner;
